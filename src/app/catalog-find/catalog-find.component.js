2d5d05c71598c6433a3d015d0e60221e9ff54bf2
VitorRoda <roda-vitor2@outlook.com>
2020-01-22 17:18:34 -0300

    .

--- src/app/catalog-find/catalog-find.component.js
+++ src/app/catalog-find/catalog-find.component.js
@@ -16,15 +16,24 @@ function CatalogFindController($scope, ngMeta, MetaService) {
   var vm = this
 
   vm.width = window.innerWidth
-  
+
   vm.colors = [
-    { color : "#724040" },
-    { color : "#d2c4b8" },
-    { color : "#b3a37f" },
-    { color : "#eaeaea" },
-    { color : "#341515" },
-    { color : "#9b9086" },
-    { color : "#d7ba77" },
-    { color : "#c9bebe" }
+    { color: "#724040" },
+    { color: "#d2c4b8" },
+    { color: "#b3a37f" },
+    { color: "#eaeaea" },
+    { color: "#341515" },
+    { color: "#9b9086" },
+    { color: "#d7ba77" },
+    { color: "#c9bebe" }
   ]
+  $scope.slider = {
+    minValue: 100,
+    maxValue: 2000,
+    options: {
+        floor: 0,
+        ceil: 100,
+        step: 1
+    }
+};
 }
