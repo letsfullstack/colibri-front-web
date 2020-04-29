125102bcbcfc4ea7d6e02989451b754832894245
Vitor Borges <vitorsbgs@gmail.com>
2020-04-29 12:28:10 -0300

    fix map, swiper and others bugs

--- src/app/contact/contact.component.js
+++ src/app/contact/contact.component.js
@@ -14,18 +14,17 @@ export const ContactComponent = {
 
 function ContactController($scope, ngMeta, MetaService) {
 
-  var map, marker, myLatLng = { lat: -23.4583392, lng: -51.426856 };
+  // var map, marker, myLatLng = { lat: -23.4583392, lng: -51.426856 };
 
-  (function initMap() {
-    map = new google.maps.Map(document.getElementById('maps'), {
-      center: myLatLng,
-      zoom: 19
-    });
+  // (function initMap() {
+  //   map = new google.maps.Map(document.getElementById('maps'), {
+  //     center: myLatLng,
+  //     zoom: 19
+  //   });
 
-    marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map
-    });
-  })()
-
+  //   marker = new google.maps.Marker({
+  //     position: myLatLng,
+  //     map: map
+  //   });
+  // })()
 }
