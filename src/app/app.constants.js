47d67229b0232af80a224e9dd1e38ca5a98bbd96
Vitor Borges <vitorsbgs@gmail.com>
2020-05-20 10:49:07 -0300

    new production url

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -6,8 +6,8 @@ export default {
         PROD: false
     },
     production: {
-        SERVER_URL: "http://prod.colibri.letscomunica.com.br/api",
-        BLOG_URL: "http://prod.colibri-blog.letscomunica.com.br",
+        SERVER_URL: "https://colibrimoveis.com.br/api",
+        BLOG_URL: "https://blog.colibrimoveis.com.br",
         PROD: true
     }
 }
