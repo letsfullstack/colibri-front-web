bcea3e9f9db03bcde88c0e7f614b583f87fd4b85
MarinaFachin <ninapinfafa@gmail.com>
2020-06-29 16:46:18 -0300

    Ajustes URL

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -1,13 +1,13 @@
 export default {
     dev: {
         //SERVER_URL: "http://localhost:3000/api",
-        SERVER_URL: "https://colibrimoveis.com.br/api",
-        BLOG_URL: "http://prod.colibri-blog.letscomunica.com.br",
+        SERVER_URL: "http://colibri.letscomunica.com.br/api/",
+        BLOG_URL: "http://blog.colibrimoveis.com.br/",
         PROD: false
     },
     production: {
-        SERVER_URL: "https://colibrimoveis.com.br/api",
-        BLOG_URL: "https://blog.colibrimoveis.com.br",
+        SERVER_URL: "http://colibri.letscomunica.com.br/api/",
+        BLOG_URL: "http://blog.colibrimoveis.com.br/",
         PROD: true
     }
 }
