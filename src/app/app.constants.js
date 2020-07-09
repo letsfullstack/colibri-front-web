9be3beae9c7433732c270be5608aae0072436fe9
Vitor Borges <vitorsbgs@gmail.com>
2020-07-09 18:19:54 -0300

    .

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -7,7 +7,7 @@ export default {
         PROD: false
     },
     production: {
-        SERVER_URL: "http://colibri.letscomunica.com.br/api/",
+        SERVER_URL: "http://colibrimoveis.com.br/api/",
         BLOG_URL: "http://blog.colibrimoveis.com.br/",
         PROD: true
     }
