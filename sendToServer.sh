0df548ef70ad8eb7cfee6d99756ff965c9bb6a9d
Junio de Souza <junioapdesouza@hotmail.com>
2022-01-27 23:37:02 -0300

    Novas anotações

---
+++ sendToServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# npm run build
+
+tar -czvf colibri.tar.gz build/
+scp colibri.tar.gz root@23.239.28.71:/var/www/colibri/colibrifrontweb
+rm -rf colibri.tar.gz
+ssh root@23.239.28.71 'cd /var/www/colibri/colibrifrontweb/ && tar -xzvf colibri.tar.gz'
