ef80b997265069b1918128ef91ead2307e1a02a9
Junio de Souza <junioapdesouza@hotmail.com>
2024-09-06 20:36:21 -0300

    new deploy

--- sendToServer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm run build
-
-tar -czvf colibri.tar.gz build/
-scp colibri.tar.gz root@23.239.28.71:/var/www/colibri/colibrifrontweb
-rm -rf colibri.tar.gz
-ssh root@23.239.28.71 'cd /var/www/colibri/colibrifrontweb/ && tar -xzvf colibri.tar.gz'
