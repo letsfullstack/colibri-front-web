7448a4fbf61203134618cfa14229159d2bea7233
Junio de Souza <junioapdesouza@hotmail.com>
2025-06-01 19:56:51 -0300

    update environment URLs and translations for catalog measures

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -3,12 +3,18 @@ export default {
 		catalogo: 'https://conteudo.colibrimoveis.com.br/colecao-2021',
 	},
 	dev: {
-		SITE_URL: "http://0.0.0.0:3009",
-		SERVER_URL: "http://0.0.0.0:3009/api/",
+		// SITE_URL: "http://0.0.0.0:3009",
+		// SERVER_URL: "http://0.0.0.0:3009/api/",
+		// BLOG_URL: "/blog",
+		// CLIENT_URL: "https://colibrimoveis.simplificamais.com.br/",
+		// STORAGE_URL: "https://www.colibrimoveis.com.br/files/",
+		// PROD: false
+		SITE_URL: "https://www.colibrimoveis.com.br",
+		SERVER_URL: "https://www.colibrimoveis.com.br/api/",
 		BLOG_URL: "/blog",
 		CLIENT_URL: "https://colibrimoveis.simplificamais.com.br/",
 		STORAGE_URL: "https://www.colibrimoveis.com.br/files/",
-		PROD: false
+		PROD: true
 	},
 	production: {
 		SITE_URL: "https://www.colibrimoveis.com.br",
