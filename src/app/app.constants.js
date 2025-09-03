0f8c85fd4ae323b6c717f9e2c340a251daf2012d
1Nix1 <thiagoaugustodeoliveiraalves@gmail.com>
2025-09-03 20:32:08 -0300

    feat: update environment constants and enhance UI components

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -2,13 +2,16 @@ export default {
 	links: {
 		catalogo: 'https://conteudo.colibrimoveis.com.br/colecao-2021',
 	},
+	local:{
+		SITE_URL: "http://0.0.0.0:3009",
+		SERVER_URL: "http://0.0.0.0:3009/",
+		BLOG_URL: "/blog",
+		CLIENT_URL: "https://colibrimoveis.simplificamais.com.br/",
+		STORAGE_URL: "https://www.colibrimoveis.com.br/files/",
+		PROD: false
+	},
 	dev: {
-		// SITE_URL: "http://0.0.0.0:3009",
-		// SERVER_URL: "http://0.0.0.0:3009/api/",
-		// BLOG_URL: "/blog",
-		// CLIENT_URL: "https://colibrimoveis.simplificamais.com.br/",
-		// STORAGE_URL: "https://www.colibrimoveis.com.br/files/",
-		// PROD: false
+		
 		SITE_URL: "https://www.colibrimoveis.com.br",
 		SERVER_URL: "https://www.colibrimoveis.com.br/api/",
 		BLOG_URL: "/blog",
